5d8577306c2e17f7e490238eb7d467554ae08b8a
movkaCreator <kpnkerzakov@gmail.com>
2024-09-01 23:14:03 +0400

    Small fix

--- src/test/java/com/aston/AppTest.java
+++ src/test/java/com/aston/AppTest.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestMts {
+public class AppTest {
     private static WebDriver driver;
 
     @BeforeAll
